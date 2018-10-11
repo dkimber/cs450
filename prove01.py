1fda4b30e76bc7cbf97277f6e50854f98db91766
Kim Dean <dea13004@byui.edu>
2018-10-10 22:18:31 -0600

    Prove01 assignment: Hardcoded classifier

--- prove01.py
+++ prove01.py
@@ -3,13 +3,12 @@ from sklearn import datasets
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 
-if __name__ == "__main__":
-    main(sys.argv)
+iris = datasets.load_iris()
 
 class HardCodedModel:
     def __init__(self, d_train, t_train):
-        self.datatrain = datatrain
-		self.targetstrain = targetstrain
+        self.datatrain = d_train
+        self.targetstrain = t_train
 
     def predict(self, datatest):
         target = []
@@ -19,22 +18,30 @@ class HardCodedModel:
 
 class HardCodedClassifier:
     def fit(self, x_train, y_train):
-        model = HardCodedModel(xtrain, ytrain)
-		return model
+        model = HardCodedModel(x_train, y_train)
+        return model
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
-iris = datasets.load_iris()
-
-# Show the data (the attributes of each instance)
-print(iris.data)
-
-# Show the target values (in numeric format) of each instance
-print(iris.target)
-
-# Show the actual target names that correspond to each number
-print(iris.target_names)
+def findPercentage(predicted, y_test):
+	count = 0
+	successes = 0
+	for x in predicted:
+		if x == y_test[count]:
+			successes = successes + 1
+		count = count + 1
+	print float('{0:.2f}'.format((float(successes)/len(y_test)) * 100)), "% correct (", successes,'/',len(y_test),")"
 
 
-classifier = GaussianNB()
-model = classifier.fit(data_train, targets_train)
-targets_predicted = model.predict(data_test)
+x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.33, random_state=42)
+
+
+gClassifier = GaussianNB()
+gModel = gClassifier.fit(x_train, y_train)
+gTargets_predicted = gModel.predict(x_test)
+print "Gaussian Estimate:"
+findPercentage(gTargets_predicted, y_test)
+
+hClassifier = HardCodedClassifier()
+hModel = hClassifier.fit(x_train, y_train)
+hTargets_predicted = hModel.predict(x_test)
+print "My Estimate:"
+findPercentage(hTargets_predicted, y_test)
